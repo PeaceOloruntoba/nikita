9e9b2240aa0fec0d66bf650ec401a7d3bbec8c8a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-05 02:16:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router';
+import React from "react";
+import { Route, Routes } from "react-router";
+import RootLayout from "./layouts/RootLayout";
+import Login from "./pages/Login";
+import Feedback from "./pages/Feedback";
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route element={<AuthLayout />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+        <Route element={<RootLayout />}>
+          <Route path="" element={<Feedback />} />
         </Route>
+        <Route path="login" element={<Login />} />
       </Routes>
     </>
   );
