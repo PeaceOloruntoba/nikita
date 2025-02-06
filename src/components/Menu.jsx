d87fab575ddb58354f8b884f1ff5f358caaf4f4f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 15:43:44 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Button from "./shared/Button";
+import {
+  MdOutlineCancel,
+  MdCheckCircleOutline,
+  MdOutlineAddCircleOutline,
+} from "react-icons/md";
 
-export default function Menu() {
+export function MenuCategory() {
   return (
-    <div>Menu</div>
-  )
+    <div className="bg-white rounded-lg p-4">
+      <div className="flex items-center gap-6">
+        <input
+          type="text"
+          className="border-none p-2 rounded-lg"
+          placeholder="Add Categorie"
+        />
+        <Button
+          className="bg-[#4895E5]"
+          value={<MdOutlineAddCircleOutline />}
+        />
+      </div>
+    </div>
+  );
+}
+export default function Menu() {
+  return <div>Menu</div>;
 }
