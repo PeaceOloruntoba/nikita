b576c78a14223c445a96dccf8c0289435a3b90e1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 15:58:10 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,6 +4,7 @@ import {
   MdOutlineCancel,
   MdCheckCircleOutline,
   MdOutlineAddCircleOutline,
+  MdModeEdit,
 } from "react-icons/md";
 
 export function MenuCategory() {
@@ -16,7 +17,7 @@ export function MenuCategory() {
           placeholder="Add Categorie"
         />
         <Button
-          className="bg-[#4895E5] text-white p-3 rounded-lg"
+          className="cursor-pointer bg-[#4895E5] text-white p-3 rounded-lg"
           value={<MdCheckCircleOutline size={18} />}
         />
       </div>
@@ -30,7 +31,7 @@ export function MenuCategory() {
           disabled
         />
         <Button
-          className="bg-[#D92C4A] text-white p-3 rounded-lg"
+          className="cursor-pointer bg-[#D92C4A] text-white p-3 rounded-lg"
           value={<MdOutlineCancel size={18} />}
         />
       </div>
@@ -42,14 +43,9 @@ export function DishCategory() {
   return (
     <div className="bg-white rounded-lg p-4 h-full text-2xl flex flex-col gap-6">
       <div className="flex items-center gap-6">
-        <input
-          type="text"
-          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
-          placeholder="Add Categorie"
-        />
         <Button
-          className="bg-[#4895E5] text-white p-3 rounded-lg"
-          value={<MdCheckCircleOutline size={18} />}
+          className="cursor-pointer bg-[#4895E5] text-white p-3 rounded-lg"
+          value={<MdOutlineAddCircleOutline size={18} />}
         />
       </div>
       <hr />
@@ -57,12 +53,16 @@ export function DishCategory() {
         <input
           type="text"
           className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
-          placeholder="Categorie 1"
-          value={"Categorie 1"}
+          placeholder="Dish 1"
+          value={"Dish 1"}
           disabled
         />
         <Button
-          className="bg-[#D92C4A] text-white p-3 rounded-lg"
+          className="cursor-pointer bg-[#AAAFB6] text-white p-3 rounded-lg"
+          value={<MdModeEdit size={18} />}
+        />
+        <Button
+          className="cursor-pointer bg-[#D92C4A] text-white p-3 rounded-lg"
           value={<MdOutlineCancel size={18} />}
         />
       </div>
