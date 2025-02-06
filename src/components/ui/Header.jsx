a0756b0d18c8adc0069361ff1d84718b35c5aa99
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 13:49:07 +0100

    Update Header.jsx

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div className='w-full'></div>
+    <div className='w-full flex p-12 items-center justify-end'></div>
   )
 }
