b931760b628eafa44f1126b0f9ad50bd8e29b999
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 16:03:03 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,6 +6,7 @@ import {
   MdOutlineAddCircleOutline,
   MdModeEdit,
 } from "react-icons/md";
+import { PiWarningCircleThin } from "react-icons/pi";
 
 export function MenuCategory() {
   return (
@@ -70,3 +71,42 @@ export function DishCategory() {
   );
 }
 
+export function AddDishCategory() {
+  return (
+    <div className="bg-white rounded-lg p-4 h-full text-2xl flex flex-col gap-6">
+      <input
+        type="text"
+        className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+        placeholder="Dish Name"
+      />
+      <div className="bg-[#D02C46] p-4 flex items-center justify-center text-white">
+        <span className="text-white">
+          <PiWarningCircleThin size={20} />
+        </span>
+        <span>
+          Achtung: Bitte wählen Sie alle Ihre Allergien aus, um sicherzustellen,
+          dass Ihren Gästen keine falschen Gerichte vorgeschlagen werden. So
+          können wir Ihren Gästen eine sichere und passende Auswahl bieten!
+        </span>
+      </div>
+      <hr />
+      <div className="flex items-center gap-6">
+        <input
+          type="text"
+          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+          placeholder="Dish 1"
+          value={"Dish 1"}
+          disabled
+        />
+        <Button
+          className="cursor-pointer bg-[#AAAFB6] text-white p-3 rounded-lg"
+          value={<MdModeEdit size={18} />}
+        />
+        <Button
+          className="cursor-pointer bg-[#D92C4A] text-white p-3 rounded-lg"
+          value={<MdOutlineCancel size={18} />}
+        />
+      </div>
+    </div>
+  );
+}
