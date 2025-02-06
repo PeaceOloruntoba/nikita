84e44ea2df62daae96610fac4b7780f8ca67436b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 15:54:25 +0100

    menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,7 +8,7 @@ import {
 
 export function MenuCategory() {
   return (
-    <div className="bg-white rounded-lg p-4 h-full">
+    <div className="bg-white rounded-lg p-4 h-full text-2xl flex flex-col gap-6">
       <div className="flex items-center gap-6">
         <input
           type="text"
@@ -16,13 +16,57 @@ export function MenuCategory() {
           placeholder="Add Categorie"
         />
         <Button
-          className="bg-[#4895E5] text-white p-2 rounded-lg"
+          className="bg-[#4895E5] text-white p-3 rounded-lg"
           value={<MdCheckCircleOutline size={18} />}
+        />
+      </div>
+      <hr />
+      <div className="flex items-center gap-6">
+        <input
+          type="text"
+          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+          placeholder="Categorie 1"
+          value={"Categorie 1"}
+          disabled
+        />
+        <Button
+          className="bg-[#D92C4A] text-white p-3 rounded-lg"
+          value={<MdOutlineCancel size={18} />}
         />
       </div>
     </div>
   );
 }
-export default function Menu() {
-  return <div>Menu</div>;
+
+export function DishCategory() {
+  return (
+    <div className="bg-white rounded-lg p-4 h-full text-2xl flex flex-col gap-6">
+      <div className="flex items-center gap-6">
+        <input
+          type="text"
+          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+          placeholder="Add Categorie"
+        />
+        <Button
+          className="bg-[#4895E5] text-white p-3 rounded-lg"
+          value={<MdCheckCircleOutline size={18} />}
+        />
+      </div>
+      <hr />
+      <div className="flex items-center gap-6">
+        <input
+          type="text"
+          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+          placeholder="Categorie 1"
+          value={"Categorie 1"}
+          disabled
+        />
+        <Button
+          className="bg-[#D92C4A] text-white p-3 rounded-lg"
+          value={<MdOutlineCancel size={18} />}
+        />
+      </div>
+    </div>
+  );
 }
+
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { MenuCategory } from "../components/Menu";
+import { DishCategory, MenuCategory } from "../components/Menu";
 
 export default function Menu() {
   return (
-    <div className="m-6 border-b border-[#4895E5]/25 p-6 grid grid-cols-2">
+    <div className="m-6 border-b border-[#4895E5]/25 p-6 grid grid-cols-2 h-full gap-12">
       <MenuCategory />
+      <DishCategory />
     </div>
   );
 }
