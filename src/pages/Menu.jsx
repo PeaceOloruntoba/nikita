f2db2584bad40568cacf4796f717e1cf263f3080
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 15:44:46 +0100

    Update Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { MenuCategory } from "../components/Menu";
 
 export default function Menu() {
   return (
-    <div className='m-6 border-b border-[#4895E5]/25 p-6'>
-      
+    <div className="m-6 border-b border-[#4895E5]/25 p-6 grid grid-cols-2">
+      <MenuCategory />
     </div>
-  )
+  );
 }
