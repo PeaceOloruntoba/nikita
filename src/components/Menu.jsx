cbddb0e901a59e5974f590dc968ff09d67a5db34
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 15:49:52 +0100

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,16 +8,16 @@ import {
 
 export function MenuCategory() {
   return (
-    <div className="bg-white rounded-lg p-4">
+    <div className="bg-white rounded-lg p-4 h-full">
       <div className="flex items-center gap-6">
         <input
           type="text"
-          className="border-none p-2 rounded-lg"
+          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
           placeholder="Add Categorie"
         />
         <Button
-          className="bg-[#4895E5]"
-          value={<MdOutlineAddCircleOutline />}
+          className="bg-[#4895E5] text-white p-2 rounded-lg"
+          value={<MdCheckCircleOutline size={18} />}
         />
       </div>
     </div>
