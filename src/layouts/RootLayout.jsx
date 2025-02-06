5b528b02e5320adee218d9c703be07c38d8251fd
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 12:13:57 +0100

    morning

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 
 export default function RootLayout() {
   return (
-    <div className="w-screen h-screen flex bg-secondary">
+    <div className="w-screen h-screen flex bg-[#F2EBF0]">
       <Sidebar />
       <div className="flex flex-col w-full">
         <Header />
