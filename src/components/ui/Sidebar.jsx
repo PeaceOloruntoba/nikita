da8ad60c90b8606a683a0bc2d07ffaf8d6afcb8a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 12:15:59 +0100

    Update Sidebar.jsx

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className='bg-primary py-12 h-screen'>
+      <div className="flex flex-col w-full gap-8 h-full">
+        <div className="flex">
+          <img src="" className='rouded-full' alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
