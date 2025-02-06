83c263e5053031241913d85e0b74be20304cd44b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 16:46:57 +0100

    Update Subscrption.jsx

--- src/components/Subscrption.jsx
+++ src/components/Subscrption.jsx
@@ -16,7 +16,7 @@ export function Cards({ value, border }) {
 export function EditSubscription({ onClose }) {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-      <div className="bg-white rounded-lg p-6 text-2xl flex flex-col gap-6 w-[400px] h-[600px]">
+      <div className="bg-white rounded-lg p-6 text-2xl flex flex-col gap-6 w-[400px]">
         <button onClick={onClose} className="self-end text-gray-600">
           <MdOutlineCancel size={24} />
         </button>
@@ -36,37 +36,22 @@ export function EditSubscription({ onClose }) {
             placeholder="Address"
           />
         </div>
-        <div className="bg-[#D02C46] p-6 rounded-lg flex items-center justify-center text-white">
-          <span className="text-white p-12">
-            <PiWarningCircleThin size={30} />
-          </span>
-          <span>
-            Achtung: Bitte wählen Sie alle Ihre Allergien aus, um
-            sicherzustellen, dass Ihren Gästen keine falschen Gerichte
-            vorgeschlagen werden. So können wir Ihren Gästen eine sichere und
-            passende Auswahl bieten!
-          </span>
+        <div className="flex flex-col">
+          <label htmlFor="">Break End Time</label>
+          <input
+            type="text"
+            className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+            placeholder="Address"
+          />
         </div>
-        <input
-          type="text"
-          className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
-          placeholder="Search ingredient"
-        />
-        <hr />
-        <ul className="flex flex-col items-center gap-4 text-[#3A3A3A] w-full h-full">
-          <li className="flex items-center justify-between w-full px-2">
-            Ingredient 1 <input type="checkbox" name="ingredient1" />
-          </li>
-          <li className="flex items-center justify-between w-full px-2">
-            Ingredient 2{" "}
-            <input
-              type="checkbox"
-              checked
-              name="ingredient2"
-              className="text-[#6C1233]"
-            />
-          </li>
-        </ul>
+        <div className="flex flex-col">
+          <label htmlFor="">Days of the week</label>
+          <input
+            type="text"
+            className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A] focus:outline-[#4895E5]"
+            placeholder="Address"
+          />
+        </div>
         <button className="cursor-pointer bg-[#4895E5] text-white p-3 rounded-lg w-full flex items-center text-center justify-center">
           <MdCheckCircleOutline size={18} />
         </button>
