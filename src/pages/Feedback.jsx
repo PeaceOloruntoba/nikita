8bbf1995d07bd58ac90e6a22f9bb8faec4a8cba7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-06 14:18:32 +0100

    Update Feedback.jsx

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { negativeFeedback, positiveFeedback, totalFeedback } from "../assets";
 
 export default function Feedback() {
   const feedbackCard = [
@@ -6,23 +7,19 @@ export default function Feedback() {
       id: 1,
       name: "Total Feedback",
       value: 200,
-      icon: <img src="" alt="" />,
+      icon: <img src={totalFeedback} alt="" />,
     },
     {
       id: 2,
-      firstName: "Surename",
-      lastName: "Name",
-      stars: 5.0,
-      feedback:
-        "Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback ",
+      name: "Positive Feedback",
+      value: 120,
+      icon: <img src={positiveFeedback} alt="" />,
     },
     {
-      id: 3,
-      firstName: "Surename",
-      lastName: "Name",
-      stars: 5.0,
-      feedback:
-        "Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback Feedback ",
+      id: 2,
+      name: "Negative Feedback",
+      value: 80,
+      icon: <img src={negativeFeedback} alt="" />,
     },
   ];
   const feedbacks = [
