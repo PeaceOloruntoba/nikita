791888a56d87b0fe7191dae18e04fded7f9f4f01
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 06:42:39 +0100

    Update Subscription.jsx

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Cards } from "../components/Subscrption";
 
 export default function Subscription() {
+  const subs = [
+    {
+      id: 1,
+      value: "3 month",
+      border: "#CD7F32",
+    },
+    {
+      id: 1,
+      value: "6 month",
+      border: "#AAAFB6",
+    },
+    {
+      id: 3,
+      value: "12 month",
+      border: "#FBBB00",
+    },
+  ];
   return (
-    <div>Subscription</div>
-  )
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="grid grid-cols-3 gap-12">
+        {subs?.map((sub) => (
+          <Cards value={sub?.value} border={sub?.value} key={sub?.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
