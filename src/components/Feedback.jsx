bafae450543e047d37cb5210c39e88a0e1b1ba73
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 14:52:25 +0100

    Update Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -21,11 +21,11 @@ export function FeedbackTitleCard({ card }) {
 export function FeedbackCard({ card }) {
   return (
     <div
-      className="p-6 rounded-lg flex flex-col w-full  items-center justify-between bg-white text-[#EDEDED]"
+      className="p-6 rounded-lg flex flex-col w-full  items-center justify-between bg-white"
       key={card?.id}
     >
       <div className="flex items-center justify-between py-2 w-full">
-        <span className="text-[#3a3a3a]/70 text-2xl font-medium">
+        <span className="text-[#3a3a3a] text-2xl font-medium">
           {card?.firstName}, {card?.lastName}
         </span>
         <span className="bg-[#FBBB00] px-4 py-1 text-white rounded-lg text-lg font-semibold flex items-center justify-center gap-2">
@@ -34,7 +34,7 @@ export function FeedbackCard({ card }) {
         </span>
       </div>
       <hr className="w-full border-[#EDEDED] my-2" />
-      <span className="text-[#3a3a3a]/30">{card?.feedback}</span>
+      <span className="text-[#3a3a3a]/70">{card?.feedback}</span>
     </div>
   );
 }
