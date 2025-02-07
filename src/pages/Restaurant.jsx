2b5bdc2ffb78bdc6ca94a465834b584538c99fb6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 12:00:50 +0100

    Update Restaurant.jsx

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/shared/Button";
+import { MdOutlineAddCircleOutline } from "react-icons/md";
 
 export default function Restaurant() {
   return (
-    <div>Restaurant</div>
-  )
+    <div className="w-full h-full flex flex-col items-center p-8">
+      <div className="flex w-full items-end justify-end">
+        <Button
+          className="cursor-pointer bg-[#4895E5] text-white px-3 py-1 rounded-lg"
+          value={
+            <span className="flex gap-2 items-center justify-center text-xl font-semibold">
+              <MdOutlineAddCircleOutline size={18} /> Add Table
+            </span>
+          }
+        />
+      </div>
+    </div>
+  );
 }
