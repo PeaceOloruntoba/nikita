386600f584f710b8645c709881f05184a3671dc9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 11:51:06 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import Menu from "./pages/Menu";
 import Questionaries from "./pages/Questionaries";
 import Signup from "./pages/Signup";
 import { Toaster } from "sonner";
+import Restaurant from "./pages/Restaurant";
 
 export default function App() {
   return (
     <>
-    <Toaster position="top-right" />
+      <Toaster position="top-right" />
       <Routes>
         <Route element={<RootLayout />}>
           <Route path="" element={<Navigate to={"/login"} />} />
@@ -22,6 +23,7 @@ export default function App() {
           <Route path="subscription" element={<Subscription />} />
           <Route path="settings" element={<Settings />} />
           <Route path="questionaries" element={<Questionaries />} />
+          <Route path="restaurant" element={<Restaurant />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
