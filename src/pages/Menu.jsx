d4427668b773cbd1c93c9a09a2db1a96b3ddf9a5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 15:46:10 +0100

    Update Menu.jsx

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -3,7 +3,7 @@ import { DishCategory, MenuCategory } from "../components/Menu";
 
 export default function Menu() {
   return (
-    <div className="m-6 border-b border-[#4895E5]/25 p-6 grid grid-cols-2 h-full gap-12">
+    <div className="m-2 border-b border-[#4895E5]/25 p-3 grid grid-cols-2 h-full gap-6">
       <MenuCategory />
       <DishCategory />
     </div>
