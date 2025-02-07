e31087338f9f7a1d1c32fc0ff35b004606204a70
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 15:41:40 +0100

    Update Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -4,15 +4,15 @@ import { FaStar } from "react-icons/fa6";
 export function FeedbackTitleCard({ card }) {
   return (
     <div
-      className="flex items-center justify-center font-semibold text-2xl"
+      className="flex items-center justify-center font-semibold text-md"
       key={card?.id}
     >
-      <div className="p-6 rounded-lg flex w-full items-center justify-between bg-white">
-        <div className="flex items-center justify-center gap-4">
+      <div className="p-3 rounded-lg flex w-full items-center justify-between bg-white">
+        <div className="flex items-center justify-center gap-2">
           {card?.icon}
           <span>{card?.name}</span>
         </div>
-        <span className="text-4xl">{card?.value}</span>
+        <span className="text-xl">{card?.value}</span>
       </div>
     </div>
   );
@@ -21,20 +21,20 @@ export function FeedbackTitleCard({ card }) {
 export function FeedbackCard({ card }) {
   return (
     <div
-      className="p-6 rounded-lg flex flex-col w-full  items-center justify-between bg-white"
+      className="p-3 rounded-lg flex flex-col w-full items-center justify-between bg-white"
       key={card?.id}
     >
-      <div className="flex items-center justify-between py-2 w-full">
-        <span className="text-[#3a3a3a] text-2xl font-medium">
+      <div className="flex items-center justify-between pb-1 w-full">
+        <span className="text-[#3a3a3a] text-md font-medium">
           {card?.firstName}, {card?.lastName}
         </span>
-        <span className="bg-[#FBBB00] px-4 py-1 text-white rounded-lg text-lg font-semibold flex items-center justify-center gap-2">
+        <span className="bg-[#FBBB00] px-2 py-1 text-white rounded-lg text-sm font-semibold flex items-center justify-center gap-1">
           <FaStar size={10} />
           {card?.stars}.0
         </span>
       </div>
-      <hr className="w-full border-[#EDEDED] my-2" />
-      <span className="text-[#3a3a3a]/70">{card?.feedback}</span>
+      <hr className="w-full border-[#EDEDED] my-1" />
+      <span className="text-[#3a3a3a]/80 text-xs">{card?.feedback}</span>
     </div>
   );
 }
