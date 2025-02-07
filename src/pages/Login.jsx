f56035640f723eb126e27c042e762e37167ffa02
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 15:03:01 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,28 +14,28 @@ export default function Login() {
       <img src={loginBg} alt="" className="w-screen h-screen object-cover" />
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="backdrop-blur  p-12 rounded-2xl shadow-xl flex flex-col w-1/3 gap-6">
-          <span className="text-primary text-5xl font-bold w-full text-center">
+          <span className="text-primary text-3xl font-bold w-full text-center">
             Login
           </span>
           <form className="flex flex-col gap-8">
             <input
               type="text"
-              className="bg-white outline-none border-none p-8 py-4 rounded-xl"
+              className="bg-white outline-none border-none p-4 py-2 rounded-xl"
               placeholder="Enter your email or username here"
             />
             <input
               type="password"
-              className="bg-white outline-none border-none p-8 py-4 rounded-xl"
+              className="bg-white outline-none border-none p-4 py-2 rounded-xl"
               placeholder="Enter your password here"
             />
-            <div className="w-full flex items-end justify-end text-2xl text-secondary font-semibold">
+            <div className="w-full flex items-end justify-end text-md text-secondary font-semibold">
               <span className="">
                 Don't have an account? <NavLink to={"/signup"}>Sign Up</NavLink>
               </span>
             </div>
             <Button
               value={"Login"}
-              className="bg-secondary rounded-xl text-white text-2xl font-semibold py-4 hover:bg-primary cursor-pointer"
+              className="bg-secondary rounded-xl text-white text-lg font-semibold py-2 hover:bg-primary cursor-pointer"
               onClick={() => handleLogin()}
             />
           </form>
