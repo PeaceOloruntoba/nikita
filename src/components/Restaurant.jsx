78aa8996ecac68847e17db69529d839fee831153
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 12:12:21 +0100

    Update Restaurant.jsx

--- src/components/Restaurant.jsx
+++ src/components/Restaurant.jsx
@@ -4,13 +4,13 @@ import { MdQrCode } from "react-icons/md";
 
 export function Card({ number, qrCode }) {
   return (
-    <div className="bg-white rounded-lg p-4 pb-36">
+    <div className="bg-white rounded-lg p-4 pb-36 border border-[#D9D9D9] cursor-pointer">
       <div className="flex items-center justify-between">
-        <span>{number}</span>
+        <span className="text-2xl text-black">{number}</span>
         <Button
-          className="bg-primary p-1 rounded-lg"
+          className="bg-primary p-1 px-2 rounded-lg text-white"
           value={
-            <span>
+            <span className="flex items-center gap-2 text-xl font-semibold">
               QR Code <MdQrCode size={18} />
             </span>
           }
