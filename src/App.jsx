555d647f55e0b5c89dd0793bccf6039d4cffea9c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 11:26:48 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import Feedback from "./pages/Feedback";
 import Settings from "./pages/Settings";
 import Subscription from "./pages/Subscription";
 import Menu from "./pages/Menu";
+import Questionaries from "./pages/Questionaries";
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route element={<RootLayout />}>
-          <Route path="" element={<Navigate to={'/login'} />} />
+          <Route path="" element={<Navigate to={"/login"} />} />
           <Route path="feedback" element={<Feedback />} />
           <Route path="menu" element={<Menu />} />
           <Route path="subscription" element={<Subscription />} />
           <Route path="settings" element={<Settings />} />
+          <Route path="questionaries" element={<Questionaries />} />
         </Route>
         <Route path="login" element={<Login />} />
       </Routes>
