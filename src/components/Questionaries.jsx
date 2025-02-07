e27c79dc2401c9de53ef87073613af5a4418e49a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-07 11:47:02 +0100

    Update Questionaries.jsx

--- src/components/Questionaries.jsx
+++ src/components/Questionaries.jsx
@@ -39,23 +39,25 @@ export function Adder() {
 }
 export function Lister() {
   return (
-    <div className="w-full h-full bg-white rounded-lg p-8">
+    <div className="w-full h-full bg-white rounded-lg p-8 text-xl">
       <div className="flex flex-col gap-4 text-[#A44B6F]/30">
         <div className="flex items-center gap-6">
-          <input
-            type="text"
-            className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A]/50 focus:outline-[#4895E5]"
-            placeholder="rwerwerwerwer rwerwerwerwer rwerwerwerwer rwerwerwerwer 1231231231"
-            disabled
-          />
+          <span
+            className="border-none outline outline-[#4895E5]/20 p-3
+            rounded-lg w-full text-[#3A3A3A]/50 focus:outline-[#4895E5]"
+          >
+            rwerwerwerwer rwerwerwerwer rwerwerwerwer rwerwerwerwer 1231231231
+          </span>
         </div>
         <div className="flex items-center gap-6">
-          <input
-            type="text"
-            className="border-none outline outline-[#4895E5]/20 p-3 rounded-lg w-full text-[#3A3A3A]/50 focus:outline-[#4895E5] text-wrap"
-            placeholder="rwerwerwerwer rwerwerwerwer rwerwerwerwer rwerwerwerwer 123123123 112312312311231231231123123123112312312311231231231"
-            disabled
-          />
+          <span
+            className="border-none outline outline-[#4895E5]/20 p-3
+            rounded-lg w-full text-[#3A3A3A]/50 focus:outline-[#4895E5]
+            text-wrap"
+          >
+            rwerwerwerwer rwerwerwerwer rwerwerwerwer rwerwerwerwer 123123123
+            112312312311231231231123123123112312312311231231231
+          </span>
         </div>
       </div>
     </div>
