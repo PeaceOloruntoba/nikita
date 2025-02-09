2608979d2055aa461258271c48882a2bd9c1a458
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-09 06:56:41 +0100

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import { loginBg } from "../assets";
-import { NavLink, useNavigate } from "react-router";
+import { NavLink, useNavigate } from "react-router-dom";
 import Button from "../components/shared/Button";
+import { toast } from "sonner";
 import useAuthStore from "../store/useAuthStore";
+import Spinner from "../components/shared/Spinner";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -10,30 +12,38 @@ export default function Login() {
     login: state.login,
     isAuthenticating: state.isAuthenticating,
   }));
+
   const [user, setUser] = useState({
     email: "",
     password: "",
   });
+
   function handleChange(e) {
-    const { name, value, type, checked } = e.target;
+    const { name, value } = e.target;
     setUser((prev) => ({
       ...prev,
-      [name]: type === "checkbox" ? checked : value,
+      [name]: value,
     }));
   }
-  function handleLogin() {
-    navigate("/feedback");
-    toast.success("Login Successful!");
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    if (!user.email || !user.password) {
+      toast.error("Please enter both email and password!");
+      return;
+    }
+    login(user, navigate);
   }
+
   return (
     <div className="w-screen h-screen relative">
       <img src={loginBg} alt="" className="w-screen h-screen object-cover" />
       <div className="absolute inset-0 flex items-center justify-center">
-        <div className="backdrop-blur  p-12 rounded-2xl shadow-xl flex flex-col w-1/3 gap-6">
+        <div className="backdrop-blur p-12 rounded-2xl shadow-xl flex flex-col w-1/3 gap-6">
           <span className="text-primary text-3xl font-bold w-full text-center">
             Login
           </span>
-          <form className="flex flex-col gap-8">
+          <form className="flex flex-col gap-8" onSubmit={handleLogin}>
             <input
               required
               type="email"
@@ -53,14 +63,14 @@ export default function Login() {
               placeholder="Enter your password here"
             />
             <div className="w-full flex items-end justify-end text-md text-secondary font-semibold">
-              <span className="">
+              <span>
                 Don't have an account? <NavLink to={"/signup"}>Sign Up</NavLink>
               </span>
             </div>
             <Button
-              value={"Login"}
+              value={isAuthenticating ? <Spinner /> : "Login"}
               className="bg-secondary rounded-xl text-white text-lg font-semibold py-2 hover:bg-primary cursor-pointer"
-              onClick={() => handleLogin()}
+              disabled={isAuthenticating}
             />
           </form>
         </div>
