92b2f9e5b686b55bb4232d0f98504c3070b3df2c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-10 09:02:24 +0100

    Update AdminGuard.jsx

--- src/guard/AdminGuard.jsx
+++ src/guard/AdminGuard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PageNotFound from "../pages/PageNotFound";
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router";
 import useAuthStore from "../store/useAuthStore";
 
 export default function AdminGuard() {
