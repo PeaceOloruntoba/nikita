59fa4124b1c0212cb5c7e2b07fb518534d5c58d1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-10 09:16:27 +0100

    Update useAdminStore.js

--- src/store/useAdminStore.js
+++ src/store/useAdminStore.js
@@ -19,38 +19,12 @@ const getFeedbacks = async (set) => {
   }
 };
 
-const signUpUser = async (user, navigate, set) => {
-  set({ isAuthenticating: true });
-  try {
-    const response = await axiosInstance.post("/register?for-admin=1", user);
-    toast.success("Account created successfully! Please login.");
-    navigate("/login");
-  } catch (error) {
-    handleError(error);
-  } finally {
-    set({ isAuthenticating: false });
-  }
-};
-
-const logoutUser = (navigate, set) => {
-  clearAuthDataFromLocalStorage();
-  set({ user: null, token: null, isAuthenticated: false });
-  toast.success("Logged out successfully!");
-  navigate("/");
-};
-
 // Zustand Store
 const useAdminStore = create((set) => {
-  const { user, token } = loadAuthDataFromLocalStorage();
 
   return {
-    user: user || null,
-    token: token || null,
-    isAuthenticating: false,
-
-    login: (user, navigate) => loginUser(user, navigate, set),
+    feedbacks: feedbacks || {},
     getFeedbacks: () => getFeedbacks(set),
-    signUp: (user, navigate) => signUpUser(user, navigate, set),
   };
 });
 
