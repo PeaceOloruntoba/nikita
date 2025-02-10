a2603d0822744ceb46d28e9cdacfeefef1a78a40
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-10 09:31:31 +0100

    Update Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -26,15 +26,15 @@ export function FeedbackCard({ card }) {
     >
       <div className="flex items-center justify-between pb-1 w-full">
         <span className="text-[#3a3a3a] text-md font-medium">
-          {card?.firstName}, {card?.lastName}
+          {card?.user?.name}, {card?.user?.surname}
         </span>
         <span className="bg-[#FBBB00] px-2 py-1 text-white rounded-lg text-sm font-semibold flex items-center justify-center gap-1">
           <FaStar size={10} />
-          {card?.stars}.0
+          {card?.rating}.0
         </span>
       </div>
       <hr className="w-full border-[#EDEDED] my-1" />
-      <span className="text-[#3a3a3a]/80 text-xs">{card?.feedback}</span>
+      <span className="text-[#3a3a3a]/80 text-xs">{card?.content}</span>
     </div>
   );
 }
