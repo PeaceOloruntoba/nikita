c9d7041c8df8796a7ce473a402c80f8355666b8d
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-02-10 09:37:35 +0100

    Create useMenuStore.js

---
+++ src/store/useMenuStore.js
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+import axiosInstance from "../utils/axiosConfig";
+import { toast } from "sonner";
+import { handleError } from "../utils/handleError";
+
+// Fetch feedbacks from API
+const getFeedbacks = async (set) => {
+  set({ isLoading: true });
+  try {
+    const response = await axiosInstance.get("/feedbacks");
+    console.log(response);
+    const data = response?.data?.data || [];
+    set({ feedbacks: data, isLoading: false });
+  } catch (error) {
+    handleError(error);
+    set({ isLoading: false });
+  }
+};
+
+// Zustand Store
+const useMenuStore = create((set) => ({
+  feedbacks: [],
+  isLoading: false,
+  getFeedbacks: () => getFeedbacks(set),
+}));
+
+export default useMenuStore;
