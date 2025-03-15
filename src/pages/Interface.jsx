750013712702a7c1cbf51fc83264d743cb49a780
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-15 16:22:21 +0100

    Update Interface.jsx

--- src/pages/Interface.jsx
+++ src/pages/Interface.jsx
@@ -7,12 +7,12 @@ export default function Interface() {
       <span className="w-full text-2xl font-semibold">Restaurant Name</span>
       <img src={restaurant} alt="" className="w-full rounded-2xl h-64" />
       <div className="flex items-center justify-center gap-8 w-full">
-        <button className="flex flex-col w-full -start">
-          <span className="w-f text-lg font-semibold">Food Menu</span>
+        <button className="flex flex-col w-full items-start">
+          <span className="text-lg font-semibold">Food Menu</span>
           <img src={menu} alt="" className="w-full rounded-2xl h-44" />
         </button>
-        <button className="flex flex-col w-full -start">
-          <span className="w-f text-lg font-semibold">Wine Menu</span>
+        <button className="flex flex-col w-full items-start">
+          <span className="text-lg font-semibold">Wine Menu</span>
           <img src={menu} alt="" className="w-full rounded-2xl h-44" />
         </button>
       </div>
