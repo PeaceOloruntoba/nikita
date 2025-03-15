78b27e166d4fae574a3022f0d95bac2c678761c4
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-15 19:38:24 +0100

    Update handleError.js

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -3,8 +3,9 @@ import { toast } from "sonner";
 export const handleError = (error) => {
   const { response } = error;
   if (response) {
-    toast.error(response.message);
+    toast.error(response.data.message);
+  } else {
+    toast.error(error?.message);
   }
-  toast.error(error?.message);
-  console.log(error)
+  console.log(error);
 };
