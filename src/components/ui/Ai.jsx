37c2c0becf173edb59e8650748e4f51905c16540
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-15 15:54:07 +0100

    Update Ai.jsx

--- src/components/ui/Ai.jsx
+++ src/components/ui/Ai.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { annawine } from "../../assets";
 
 export default function Ai() {
   return (
     <div className="h-screen flex items-center justify-center text-xl font-semibold relative">
-      <img src="" alt="" className="" />
+      <img src={annawine} alt="" className="" />
       Ai
     </div>
   );
