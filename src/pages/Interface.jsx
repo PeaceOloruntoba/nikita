da8b49f2f8aeefdc15b00e885ceca7ade49f6ca4
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-15 16:13:49 +0100

    Update Interface.jsx

--- src/pages/Interface.jsx
+++ src/pages/Interface.jsx
@@ -3,8 +3,9 @@ import { restaurant } from "../assets";
 
 export default function Interface() {
   return (
-    <div className="flex flex-col w-full items-center justify-center gap-8">
-      <img src={restaurant} alt="" className="w-full" />
+    <div className="flex flex-col w-full items-center justify-center gap-8 p-8">
+      <span className="w-full text-2xl font-semibold">Restaurant Name</span>
+      <img src={restaurant} alt="" className="w-full rounded-2xl h-64" />
     </div>
   );
 }
