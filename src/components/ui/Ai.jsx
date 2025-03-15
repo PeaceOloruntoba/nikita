412cac78d49602f9291d92bdb30b892ea493050c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-15 16:01:03 +0100

    gh

--- src/components/ui/Ai.jsx
+++ src/components/ui/Ai.jsx
@@ -4,8 +4,16 @@ import { annawine } from "../../assets";
 export default function Ai() {
   return (
     <div className="h-screen flex items-center justify-center text-xl font-semibold relative">
-      <img src={annawine} alt="" className="" />
-      Ai
+      <img src={annawine} alt="" className="w-full h-full" />
+      <div className="absolute w-full bottom-0">
+        <div className="bg-transparent p-4 w-full">
+          <input
+            type="text"
+            placeholder="Chat with Annawine"
+            className="w-full bg-white/40 border border-primary outline-none rounded-lg p-2"
+          />
+        </div>
+      </div>
     </div>
   );
 }
