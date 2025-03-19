8559011e28c8535eb01536ce23252a22f147788e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-19 16:52:54 +0100

    Update FoodMenu.jsx

--- src/pages/FoodMenu.jsx
+++ src/pages/FoodMenu.jsx
@@ -10,7 +10,10 @@ export default function FoodMenu() {
 
   return (
     <div className="max-w-3xl mx-auto bg-white shadow-md rounded-lg p-6 mt-10 m-6">
+      <div className="flex items-center justify-between">
       <h2 className="text-2xl font-semibold text-primary mb-4">Food Menu</h2>
+      <button className="bg-primary text-white px-6 py-1 rounded cursor-pointer focus:bg-primary/70 active:bg-primary/70 ">{foodMenu.length === 0 ? "Add Food Menu" : "Update Food Menu"}</button>
+      </div>
 
       {foodMenu.length === 0 ? (
         <p className="text-gray-500">No menu items available.</p>
