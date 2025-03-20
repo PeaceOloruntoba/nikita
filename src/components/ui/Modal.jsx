73c4643c2bd7578d476de616e39f497456afc360
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-20 11:49:20 +0100

    Update Modal.jsx

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -22,7 +22,7 @@ const Modal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="fixed inset-0 bg-black/70 bg-opacity-50 flex justify-center items-center">
       <div ref={modalRef} className="bg-white p-6 rounded-lg shadow-md">
         {children}
       </div>
