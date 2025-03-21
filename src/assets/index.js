ce4d91ab7d879d42e0e21921cc5846b190c072a8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-21 21:11:38 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import profilePic from "./images/profile-pics.png";
-import loginBg from "./images/login-bg.png";
+import loginBg from "./images/login-bg.jpg";
 import totalFeedback from "./images/feedback-icon.png";
 import positiveFeedback from "./images/positive-feedback-icon.png";
 import negativeFeedback from "./images/negative-feedback-icon.png";
