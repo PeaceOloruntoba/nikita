da40b97fecab563e6cc93964d789481f848b7091
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-21 18:41:32 +0100

    Update Subscription.jsx

--- src/pages/Subscription.jsx
+++ src/pages/Subscription.jsx
@@ -3,7 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import SubscriptionForm from "../components/SubscrptionForm";
 
-const stripePromise = loadStripe(process.env.VITE_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const SubscriptionPage = () => {
   return (
