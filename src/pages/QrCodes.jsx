40b8b62aa8f042f6d33048604e274e228a1550ff
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-21 23:12:35 +0100

    ok

--- src/pages/QrCodes.jsx
+++ src/pages/QrCodes.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import useMenuStore from "../store/useMenuStore";
 import QRCode from "react-qr-code";
 import Modal from "../components/ui/Modal";
+import { jsPDF } from "jspdf";
+import qrcode from "qrcode";
 
 export default function QrCodes() {
   const { tables, getTables, updateTables } = useMenuStore();
@@ -33,6 +35,29 @@ export default function QrCodes() {
     handleCloseModal();
   };
 
+  const handlePrintQRCode = (qrValue, tableName) => {
+    qrcode.toCanvas(qrValue, { width: 512 }, (error, canvas) => {
+      if (error) {
+        console.error("Error generating QR code:", error);
+        return;
+      }
+      const pdf = new jsPDF();
+      const imgData = canvas.toDataURL("image/png");
+      const pageWidth = pdf.internal.pageSize.getWidth();
+      const pageHeight = pdf.internal.pageSize.getHeight();
+      const imageWidth = 200;
+      const imageHeight = 200;
+      const x = (pageWidth - imageWidth) / 2;
+      const y = (pageHeight - imageHeight) / 2 - 20;
+
+      pdf.addImage(imgData, "PNG", x, y, imageWidth, imageHeight);
+      pdf.text(tableName, pageWidth / 2, y + imageHeight + 15, {
+        align: "center",
+      });
+      pdf.save(`${tableName}.pdf`);
+    });
+  };
+
   return (
     <div className="max-w-3xl mx-auto bg-white shadow-md rounded-lg p-6 mt-10 m-6">
       <div className="flex items-center justify-between">
@@ -57,13 +82,26 @@ export default function QrCodes() {
               key={item?.table_number}
               className="p-4 border border-gray-300 rounded-md text-center"
             >
-              <QRCode
-                value={`profileId=${item.profile_id}&tableNumber=${item.table_number}`}
-                size={128}
-              />
-              <p className="mt-2 text-lg font-semibold">
-                Table {item.table_number}
-              </p>
+              <div className="flex flex-col items-center">
+                <QRCode
+                  value={`profileId=${item.profile_id}&tableNumber=${item.table_number}`}
+                  size={128}
+                />
+                <p className="mt-2 text-lg font-semibold">
+                  Table {item.table_number}
+                </p>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+                  onClick={() =>
+                    handlePrintQRCode(
+                      `profileId=${item.profile_id}&tableNumber=${item.table_number}`,
+                      `Table ${item.table_number}`
+                    )
+                  }
+                >
+                  Print
+                </button>
+              </div>
             </div>
           ))}
         </div>
