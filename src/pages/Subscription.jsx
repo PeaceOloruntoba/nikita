db3c0ccbe8f54816476b6c8a979aedd94096f88b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-21 18:34:49 +0100

    Create Subscription.jsx

---
+++ src/pages/Subscription.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import SubscriptionForm from "../components/SubscrptionForm";
+
+const stripePromise = loadStripe(process.env.VITE_STRIPE_PUBLISHABLE_KEY);
+
+const SubscriptionPage = () => {
+  return (
+    <Elements stripe={stripePromise}>
+      <SubscriptionForm />
+    </Elements>
+  );
+};
+
+export default SubscriptionPage;
