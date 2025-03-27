3cd70ecd3b75189a3bbf7a2b89359a2676271e93
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-03-27 11:05:48 +0100

    Update RootLayout.jsx

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,7 +7,7 @@ import Ai from "../components/ui/Ai";
 export default function RootLayout() {
   return (
     <div className="w-screen h-screen flex bg-[#F2EBF0]">
-      <div className="w-2/5">
+      <div className="w-1/4">
         <Sidebar />
       </div>
       <div className="overflow-scroll hide-scrollbar w-5/5 px-4">
