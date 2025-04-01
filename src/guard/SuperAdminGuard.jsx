4f7116f5038a4528c1123fb0cfb3a30756e596c6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-01 18:17:49 +0100

    Create SuperAdminGuard.jsx

---
+++ src/guard/SuperAdminGuard.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { Navigate, Outlet, useNavigate } from "react-router";
+import useAuthStore from "../store/useAuthStore";
+import { useEffect } from "react";
+
+export default function SuperAdminGuard() {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user.roles !== "admin") {
+      navigate("/login");
+    }
+  }, [user, navigate]); // Depend on user and navigate
+
+  // if (!user || user?.roles !== "admin") {
+  //   return null; // or a loading state. prevent outlet.
+  // }
+
+  return <Outlet />;
+}
