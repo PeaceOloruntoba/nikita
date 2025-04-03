999e7b097b2c753ad7a43700929efce84155b056
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-03 19:25:25 +0100

    Update AdminLayout.jsx

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Sidebar from "../components/ui/Sidebar";
 import { Outlet } from "react-router";
+import AdminSidebar from "../components/ui/AdminSidebar";
 
 export default function AdminLayout() {
   return (
     <div className="w-screen h-screen flex bg-[#F2EBF0]">
       <div className="w-1/4">
-        <Sidebar />
+        <AdminSidebar />
       </div>
       <div className="overflow-scroll hide-scrollbar w-5/5 px-4">
         <Outlet />
