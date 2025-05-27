4ae7e2f3170684ae09b14536f697bd7a20c89cf1
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-27 12:20:13 +0100

    Create UserGuard.jsx

---
+++ src/guard/UserGuard.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { Navigate, Outlet, useNavigate } from "react-router";
+import useAuthStore from "../store/useAuthStore";
+import { useEffect } from "react";
+
+export default function AdminGuard() {
+  const { user } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user || user?.role !== "user") {
+      navigate("/login");
+    }
+  }, [user, navigate]); // Depend on user and navigate
+
+  // if (!user || user?.roles !== "admin") {
+  //   return null; // or a loading state. prevent outlet.
+  // }
+
+  return <Outlet />;
+}
